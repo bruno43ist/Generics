20d46e5b900099bb8c5767ebcabd7b60e56e4ece
Bruno Farias <bruno43ist@yahoo.com.br>
2019-07-26 14:35:31 -0300

    type safety

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,31 @@
 package application;
 
+import java.util.Scanner;
+
+import services.PrintService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService<String> ps = new PrintService<>();
+		
+		System.out.println("How many values? ");
+		int n = sc.nextInt();
+		
+		//ps.addValue("Maria");
+		
+		for(int i = 0; i < n; i++) {
+			String value = sc.next();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		String x = ps.first();
+		System.out.println("First: " + x);
 
+		sc.close();
 	}
 
 }
